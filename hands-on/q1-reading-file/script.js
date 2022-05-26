ff82a0113b981d7b5757f43b81bf6dded866cc71
Wesley Cheah <wesley_188@yahoo.com>
2022-05-26 07:31:00 +0000

    Completed q1 with notes

--- hands-on/q1-reading-file/script.js
+++ hands-on/q1-reading-file/script.js
@@ -1,3 +1,4 @@
+// METHOD 1:
 // document.querySelector('#load-btn').addEventListener('click', function () {
 //     let response = axios.get('contact.txt').then(function (response) {
 
@@ -9,6 +10,7 @@
 // })
 
 
+// METHOD 2:
 // // USING ASYNC/AWAIT
 // document.querySelector('#load-btn').addEventListener('click', load);
 
@@ -20,9 +22,12 @@
 //     div.innerHTML = response.data;
 // }
 
+
+// METHOD 3:
 // USING ASYNC/AWAIT
 document.querySelector('#load-btn').addEventListener('click', async function () {
     let response = await axios.get('contact.txt');
+    // console.log(response.data); // -> check that we have retrieved the response
 
     // Select div#content
     let div = document.querySelector('#content');
