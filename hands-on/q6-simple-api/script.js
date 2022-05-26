a09b5f5371d3789d6e0724ec84ea49dc885cf061
Wesley Cheah <wesley_188@yahoo.com>
2022-05-26 08:40:20 +0000

    Completed q6

--- hands-on/q6-simple-api/script.js
+++ hands-on/q6-simple-api/script.js
@@ -1,2 +1,27 @@
+let url = "https://programming-quotes-api.herokuapp.com/quotes/random";
 
-let url = "https://programming-quotes-api.herokuapp.com/quotes/random"
+document.querySelector('#btnLoad').addEventListener('click', async () => {
+    let response = await axios.get(url);
+
+    let quote = response.data;
+
+    // Create <figure> element
+    let figure = document.createElement('figure');
+
+    // Create <blockquote> element
+    let blockquote = document.createElement('blockquote');
+    blockquote.innerHTML = `"${quote.en}"`;
+
+    // Create <figcaption> element
+    let figcaption = document.createElement('figcaption');
+    figcaption.innerHTML = `- ${quote.author}`;
+
+    figure.appendChild(blockquote);
+    figure.appendChild(figcaption);
+
+    // Reset div #content 's innerHTML
+    let div = document.querySelector('#content');
+    div.innerHTML = '';
+    div.appendChild(figure);
+
+});
