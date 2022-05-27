b4606920971da1a3c21b185d57b19a678e08308d
Wesley Cheah <wesley_188@yahoo.com>
2022-05-27 04:01:19 +0000

    Completed q5 with notes

--- hands-on/q5-array-of-json-objects/script.js
+++ hands-on/q5-array-of-json-objects/script.js
@@ -51,3 +51,37 @@ document.querySelector('#btnSearch').addEventListener('click', async function() 
 // ctrl + r -> to force reload if browser is caching your javascript (so latest javascript is not used)
 
 // Refactoring using functions to make code more readable
+// -> NOTE: function names should be semantic
+
+// NOTE: If an element is already linked to the DOM, any changes to its innerHTML will cause the DOM to refresh
+// -> recommend to append element to DOM after processing all the innerHTML required
+
+// Alternative method using both createElement and innerHTML:
+// document.querySelector('#load-users-btn').addEventListener('click', async function() {
+//     // Load users
+//     let response = await axios.get('users.json');
+//     let users = response.data.users;
+
+//     // Create a <ul> element
+//     let ul = document.createElement('ul');
+
+//     // Load in first name, lastname and email of each user
+//     for (let user of users) {
+//         // Create <li> element
+//         let li = document.createElement('li');
+//         li.innerHTML = `
+//         <ul>
+//             <li>First name: ${user.firstName}</li>
+//             <li>Last name: ${user.lastName}</li>
+//             <li>email: ${user.emailAddress}</li>
+//         </ul>
+//         `;
+
+//         // Append to <ul>
+//         ul.appendChild(li);
+//     }
+
+//     // Select div #all-users
+//     let div = document.querySelector('#all-users');
+//     div.appendChild(ul);
+// });
