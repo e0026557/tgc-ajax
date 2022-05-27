7c8c7d92f2be3d6b030efe02df790495a8f8d3a1
Wesley Cheah <wesley_188@yahoo.com>
2022-05-27 02:58:00 +0000

    Completed q2 with notes

--- hands-on/q2-read-file-as-items/script.js
+++ hands-on/q2-read-file-as-items/script.js
@@ -1,6 +1,7 @@
 document.querySelector('#btnLoad').addEventListener('click', async ()=> {
-    let response = await axios.get('items.txt');
-    console.log(response.data);
+    // Note: Since script.js and items.txt are in the same directory
+    let response = await axios.get('items.txt'); // -> return Promise (process running in background) by default without 'await'
+    // console.log(response.data);
     let fruitArr = response.data.split(',');
 
     // Create <ul> element
@@ -18,3 +19,29 @@ document.querySelector('#btnLoad').addEventListener('click', async ()=> {
     // Append <ul> to div #content
     document.querySelector('#content').appendChild(ul);
 });
+
+// NOTE: querySelector takes time to search through DOM for an element, so it is recommended not to put in inside a loop.
+// -> Optimise this by declaring a variable and caching this element object into the variable instead of calling querySelector multiple times
+
+// Advanced method: Using map
+// document.querySelector('#btnLoad').addEventListener('click', async function() {
+//     let response = await axios.get('items.txt');
+//     let fruitArr = response.data.split(',');
+
+//     // Create <ul> element
+//     let ul = document.createElement('ul');
+
+//     // Map each element in fruitArr into an array of <li> elements
+//     let liElements = fruitArr.map(function(fruit) {
+//         let liElement = document.createElement('li');
+//         liElement.innerHTML = fruit;
+//         return liElement;
+//     });
+
+//     for (let liElement of liElements) {
+//         ul.appendChild(liElement);
+//     }
+
+//     // Append <ul> to div #content
+//     document.querySelector('#content').appendChild(ul);
+// })
