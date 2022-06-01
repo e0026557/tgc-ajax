1db3bffc3b9b65b156c77eec2801c8923321552d
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-06-01 02:14:47 +0000

    To continue with q8

--- hands-on/q8-random-users/script.js
+++ hands-on/q8-random-users/script.js
@@ -1,1 +1,12 @@
-let url = "https://randomuser.me/api/?results=10"
+const URL = "https://randomuser.me/api/?results=10";
+
+document.querySelector('#btnLoad')
+.addEventListener('click', async function() {
+    let response = await axios.get(URL);
+    let users = response.data.results;
+
+    for (let user of users) {
+        // Create ul element
+        let ulElement = document.createElement('ul');
+    }
+});
